0263438266a260b79debf819f56522c3f25a253c
jungyeol12 <jbae23233@gmail.com>
2024-05-27 10:39:13 +0900

    2024-05-27 응화 수업
    기능건설 유형 문제 풀이

---
+++ 기능건설/js/script.js
@@ -0,0 +1,44 @@
+$(".menu").hover(
+    function() {
+        $(".sub-menu").stop().slideDown("slow");
+    },
+    function() {
+        $(".sub-menu").stop().slideUp("fast");
+    }
+)
+
+var slide = $(".slide > img");
+var index = 0;
+var size = slide.length - 1;
+var timer = setInterval("autoslide()", 1500);
+
+function autoslide() {
+    $(slide[index]).stop().animate({
+        opacity: 0
+    }, 1000);
+    index++;
+    if(index > size) index=0;
+    $(slide[index]).stop().animate({
+        opacity: 1
+    }, 1000);
+}
+
+$(".tab ul li:first-child").click(
+    function() {
+        $(this).addClass('on');
+        $(".tab ul li:last-child").removeClass('on');
+        
+        $(".gallery").hide();
+        $(".notice").show();
+    }
+);
+
+$(".tab ul li:last-child").click(
+    function() {
+        $(this).addClass('on');
+        $(".tab ul li:first-child").removeClass('on');
+    
+        $(".gallery").show();
+        $(".notice").hide();
+    }
+);
